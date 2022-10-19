8500fb024aa3d906fa7176f364c07da0f98037f8
BIRRuk <biruk.mudula@gmail.com>
2022-10-19 21:49:20 +0300

    optimizations to app.py

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'dcm'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-import torch    
-import torchvision.transforms as tfms
-
 def predict(img):
+    # img = tfms.Grayscale()(img)
+    # img = tfms.ToTensor()(img)
+    # img = img.unsqueeze(0)
+
     return {
-        'probablities': [.35, .81],
-        'dxlist': ['pneumonia', 'abnormal XR not otherwise specified'],
+        # 'probablities': [.35, .914],
+        # 'dxlist': ['pneumonia', 'abnormal XR not otherwise specified'],
         'prediction': ['abnormal XR not otherwise specified',  .81],
         'prediction_all': [
             ['pneumonia', .35], 
@@ -37,73 +38,72 @@ def hello():
 
 @app.route('/result/', methods=['GET', 'POST'])
 def result():
-    configs['title'] = f'{configs["appName"]} result'
 
     if request.method == "POST":
+        configs['title'] = f'{configs["appName"]} result'
         if 'file' not in request.files:
-            flash('No file part')
+            flash('Error: No file uploaded')
             return redirect(request.url)
 
         file = request.files['file'] #<FileStorage: 'mpv-shot0007.jpg' ('image/jpeg')>
         print(f'[ {__file__} ]', file)
 
         if file.filename == '':
-            flash('No image selected for uploading')
+            flash('Error: No file uploaded')
             return redirect(request.url)
 
         if file and allowed_file(file.filename):
 
-            try:
-                api_url = 'http://127.0.0.1:8081/api'
-                # r = requests.post(api_url, files={'file':request.files}, headers={'Content-Type':'multipart/form-data'})
-                # r = requests.post(api_url, files={'file': file}, headers={'Content-Type':'multipart/form-data'})
-                # r = requests.post(api_url, files={'file': 'file'}, headers={'Content-Type':'multipart/form-data'})
-                # r = requests.post(api_url, files=[('file', file)], headers={'Content-Type':'multipart/form-data'})
-                r = requests.post(api_url, files=[('file', file)], headers={'Content-Type':'multipart/form-data'})
-                print(r)
-            except Exception as e:
-                # raise
-                pass
-
-            img = Image.open(file).convert('L') # PIL image
-            print(f'[ {__file__} ] img size', img.size)
-            sr = img.size[0]/img.size[1]
-            if sr>1.5 or sr<.666:
-                flash('Bad aspect ratio')
+            if file.filename.rsplit('.', 1)[1].lower() in ['dcm', 'dicom']:
+                flash('Error: DICOM support is on the way, use other image formats for now')
                 return redirect(request.url)
 
-            if sr >= 1: cr = img.size[1]
-            else: cr = img.size[0]
+            else:
+                try:
+                    img = Image.open(file).convert('L') # PIL image
+                except Exception as e:
+                    flash(f'Error: Your file {file.filename} could not be opended.')
+                    return redirect(request.url)
+                    # raise e
 
-            img = tfms.Compose((
-                # tfms.Grayscale(),
-                tfms.CenterCrop(cr),
-                tfms.Resize(224),
-                tfms.ToTensor(),
-            ))(img).unsqueeze(0)
-            print(img.shape)
-            
+                im_sze = img.size
+                print(f'[ {__file__} ] img size', im_sze)
+
+                aspect = im_sze[0]/im_sze[1]
+                if aspect>1.5 or aspect<.666:
+                    flash(f'Error: Bad aspect ratio aspect ratio {im_sze}, {round(aspect,3)}x')
+                    return redirect(request.url)
+
+                sz_min = min(im_sze)
+                if sz_min < 224:
+                    flash(f'Error: Image dimenssions size too small {im_sze}')
+                    return redirect(request.url)
+
+                crop_x = (im_sze[0]-sz_min)//2
+                crop_y = (im_sze[1]-sz_min)//2
+
+                img = img.crop(box=(crop_x, crop_y, crop_x+sz_min, crop_y+sz_min))
+                img = img.resize((224,224))
+
+    
             prediction = predict(img)
-            dx, confidence = prediction['prediction']
-            confidence = round(confidence*100, 2)
             configs['prediction'] = prediction
 
             bytesio = io.BytesIO()
-            # image = image.resize((677,486))
-            image = tfms.ToPILImage()(img[0])
-            image.save(bytesio, format="JPEG")
+            img.save(bytesio, format="JPEG")
             img_base64 = base64.b64encode(bytesio.getvalue()).decode('ascii')
             configs['img_base64'] = img_base64
 
-            flash('Image successfully uploaded and displayed')
-            # print(configs)
+            print(f'[ {__file__} ]', 'Image successfully uploaded and displayed')
             return render_template('result.html', configs=configs)
 
         else:
-            flash('Allowed image types are -> png, jpg, jpeg, gif, dcm')
+            flash(f'Error: Allowed file types are: {ALLOWED_EXTENSIONS}')
             return redirect(request.url)
         
-    else: return render_template('index.html', configs=configs)
+    else:
+        configs['title'] = f'{configs["appName"]}'
+        return render_template('index.html', configs=configs)
 
 @app.route('/about/')
 def about():
